fc59c482bf9aaec06eacd3daa848fbc87e6ec25d
Sean Yasnogorodski <sean.yasno@gmail.com>
2021-04-10 13:58:45 +0300

    Init simple server.

--- src/app.js
+++ src/app.js
@@ -1,1 +1,16 @@
-console.log('Hello, world!');
+const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const PORT = process.env.PORT || 4200;
+
+const app = express();
+
+app.get('/ping', (req, res) => {
+    res.send('pong');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is up and running at port ${PORT}`);
+});
