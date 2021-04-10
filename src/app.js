fa2c7a64c91a164589277cf0f20ebaaac855cdf9
Sean Yasnogorodski <sean.yasno@gmail.com>
2021-04-10 22:56:46 +0300

    Added filter route and ratings route.

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const csv = require('csv-parser');
 const dotenv = require('dotenv');
 const _ = require('lodash');
 const fs = require('fs');
+const { lowerCase } = require('lodash');
 
 dotenv.config();
 
@@ -52,6 +53,25 @@ app.get('/data/countries', async (req, res) => {
 
 app.get('/data/categories', async (req, res) => {
     res.json(_.uniq(_.flatMap(_.uniq(data.map(item => item.listed_in.split(', ').map(i => i.replace(',', ''))))).sort()));
+});
+
+app.get('/data/ratings', async (req, res) => {
+    res.json(_.uniq(data.map(item => item.rating).sort()));
+});
+
+app.get('/data/filter/:filter/:value', async (req, res) => {
+    const {filter = '', value = ''} = req.params;
+    const filters = ['type', 'title', 'director', 'country', 'release_year', 'rating', 'duration'];
+
+    if (_.isEmpty(filter)) {
+        return res.status(406).json({error: 'Filter cannot be empty.'});
+    } else if (_.isEmpty(value)) {
+        return res.status(406).json({error: 'Value cannot be empty.'});
+    } else if (filters.find(item => item === filter)) {
+        return res.json(data.filter(item => lowerCase(item[filter]) === lowerCase(value)));
+    } else {
+        return res.status(406).json({error: 'Wrong filter.'});
+    }
 });
 
 app.listen(PORT, async () => {
